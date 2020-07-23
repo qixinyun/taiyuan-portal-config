f62982d0f3ab7b9c908c60eb68b25593c12a745c
li_jia <1229844289@qq.com>
2020-07-23 16:07:06 +0800

    v0.1.0

---
+++ src/SmartyConfig/Common/SmartyConfig.php
@@ -0,0 +1,16 @@
+<?php
+namespace SmartyConfig\Common;
+
+class SmartyConfig
+{
+    public static function configDir()
+    {
+        $dirArray = array(
+            S_ROOT.'vendor/qixinyun/xx-portal-config/src/SmartyConfig/Ty',
+            S_ROOT.'vendor/qixinyun/xx-portal-config/src/SmartyConfig/Common',
+            S_ROOT.'vendor/qixinyun/xx-portal-config/src/SmartyConfig/',
+        );
+
+        return $dirArray;
+    }
+}
